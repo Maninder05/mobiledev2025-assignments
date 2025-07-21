3869649c5944ecfff4ba67d2eb7710cb19e325c7
evanandal <Eva.Nandal@edu.sait.ca>
2025-07-21 12:50:53 -0600

    Update signin.tsx

--- CRUD_Oprt/app/signin.tsx
+++ CRUD_Oprt/app/signin.tsx
@@ -129,34 +129,3 @@ const styles = StyleSheet.create({
 });
  
  
- 
- 
- 
- 
- 
-signin.tsx
- 
-import { Slot } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
- 
-export default function Layout() {
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <View style={styles.container}>
-          <Slot />
-        </View>
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
-  );
-}
- 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
- 
- 
